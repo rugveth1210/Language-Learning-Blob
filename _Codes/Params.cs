d600508e0563f3d5a23465ad88c2e1f368d0375b
rugveth1210 <62737575+rugveth1210@users.noreply.github.com>
2023-02-28 14:28:35 -0600

    Update Params.cs

--- _Codes/Params.cs
+++ _Codes/Params.cs
@@ -1,16 +1,14 @@
 using System;  
-namespace AccessSpecifiers{  
-    class Params{  
+class Params{  
 
-        public void display(params int[] val){  
-            for (int i=0; i<val.Length; i++){  
-                Console.WriteLine(val[i]);  
-            }  
+    public void display(params int[] val){  
+        for (int i=0; i<val.Length; i++){  
+            Console.WriteLine(val[i]);  
         }  
-          
-        public static void Main(string[] args){  
-            Params param = new Params(); 
-            param.display(1,5,7,82,16);
-        }  
+    }  
+        
+    public static void Main(string[] args){  
+        Params param = new Params(); 
+        param.display(1,5,7,82,16);
     }  
 }  
