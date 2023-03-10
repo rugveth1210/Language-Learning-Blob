8b2ba64115b97f0b0e6791178d23ae0855c3c6aa
rugveth1210 <62737575+rugveth1210@users.noreply.github.com>
2023-03-10 15:06:18 -0600

    Add files via upload

---
+++ _Codes/Student.cs
@@ -0,0 +1,14 @@
+using System;  
+public class Student{  
+    int id;
+    String name;  
+        
+    public static void Main(string[] args){  
+        Student s1 = new Student();   
+        s1.id = 1;  
+        s1.name = "Sai Rugveth";  
+        Console.WriteLine(s1.id);  
+        Console.WriteLine(s1.name);  
+
+    }  
+}  
